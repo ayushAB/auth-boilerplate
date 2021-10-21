d1ddb899f8811f0f560abb59460fe1596bb8ce5b
ayushAB <ayushbante6@gmail.com>
2021-10-21 17:27:46 +0530

    auth js changes

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ require('dotenv').config()
 const express = require('express');
 const dbConnect = require('./Configurations/database_configuration');
 const app = express();
-var cors = require('cors');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+require('./Models/user')
+require('./Configurations/passport_configuration');
+
 const port = process.env.PORT || 4000
 app.use(cors({optionsSuccessStatus: 200})); 
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(session({ secret: 'passport-tutorial', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
 //Database connection
 const connectionURL = process.env.DB_URL || 'mongodb://localhost:27017/myapp'
 dbConnect(connectionURL)
